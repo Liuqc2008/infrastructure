a654525ff35c648c1e45c2d8a80a83fe01bf7df5
Liuqc2008 <313624981@qq.com>
2018-10-23 16:39:18 +0800

    第一次提交

---
+++ src/test/java/infrastructure/test/StringTest.java
@@ -0,0 +1,17 @@
+package infrastructure.test;
+
+
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+public class StringTest {
+
+	@Test
+	public void test() {
+		String dd = " dds s ";
+		
+		System.out.println(StringUtils.trim(dd));
+	}
+
+}

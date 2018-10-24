b25737fc8d690135d341a31f9bfb8c179e2d6e03
Liuqc2008 <313624981@qq.com>
2018-10-24 17:21:52 +0800

    增加exception

---
+++ src/main/java/infrastructure/exception/AlertException.java
@@ -0,0 +1,14 @@
+package infrastructure.exception;
+
+
+public class AlertException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AlertException( String message){
+		
+		super(message);
+	}
+}
